89d4220fc6dbb61e65cfd21384c47deaf799893c
Julian Alberts <julian.alberts@ewe.net>
2022-06-19 18:18:19 +0200

    Updated to mini_template v0.2

--- i3_notify_bar/src/template.rs
+++ i3_notify_bar/src/template.rs
@@ -1,32 +1,13 @@
-use std::collections::HashMap;
-
 use crate::notification_bar::NotificationTemplateData;
 
-use mini_template::{value::Value, MiniTemplate};
+use mini_template::{MiniTemplate, ValueManager};
 
 pub const DEFAULT_TEMPLATE_ID: u64 = 0;
 
-static mut TEMPLATE_MANAGER: Option<MiniTemplate<u64>> = None;
+static mut TEMPLATE_MANAGER: Option<MiniTemplate> = None;
 static mut NEXT_TEMPLATE_ID: u64 = DEFAULT_TEMPLATE_ID + 1;
 
 pub fn render_template(tpl_id: &u64, context: &NotificationTemplateData) -> String {
-    let mut data = HashMap::with_capacity(5);
-
-    data.insert(
-        "app_name".to_string(),
-        Value::String(context.app_name.to_owned()),
-    );
-    data.insert("body".to_string(), Value::String(context.body.to_owned()));
-    data.insert(
-        "expire_timeout".to_string(),
-        Value::Number(context.expire_timeout as f64),
-    );
-    data.insert("icon".to_string(), Value::String(context.icon.to_owned()));
-    data.insert(
-        "summary".to_string(),
-        Value::String(context.summary.to_owned()),
-    );
-
     unsafe {
         let tplm = match &TEMPLATE_MANAGER {
             Some(tm) => tm,
@@ -38,7 +19,7 @@ pub fn render_template(tpl_id: &u64, context: &NotificationTemplateData) -> Stri
                 }
             }
         };
-        let output = match tplm.render(tpl_id, &data) {
+        let output = match tplm.render(tpl_id.to_string().as_str(), ValueManager::from_serde(context).unwrap()) {
             Ok(s) => s,
             Err(e) => e.to_string(),
         };
@@ -48,6 +29,7 @@ pub fn render_template(tpl_id: &u64, context: &NotificationTemplateData) -> Stri
 
 pub fn add_template(template: String) -> Result<u64, ()> {
     unsafe {
+        let id = NEXT_TEMPLATE_ID.to_string();
         let old_template = match &mut TEMPLATE_MANAGER {
             Some(tm) => tm,
             None => {
@@ -58,7 +40,7 @@ pub fn add_template(template: String) -> Result<u64, ()> {
                 }
             }
         }
-        .add_template(NEXT_TEMPLATE_ID, template);
+        .add_template(id, template);
 
         match old_template {
             Ok(None) => {
@@ -71,10 +53,10 @@ pub fn add_template(template: String) -> Result<u64, ()> {
     }
 }
 
-fn init_template_manager() -> MiniTemplate<u64> {
+fn init_template_manager() -> MiniTemplate {
     let mut tplm = MiniTemplate::default();
     tplm.add_default_modifiers();
-    if let Err(_) = tplm.add_template(0, "[{app_name}] {summary}: {body}".to_owned()) {
+    if let Err(_) = tplm.add_template("0".to_owned(), "[{app_name}] {summary}: {body}".to_owned()) {
         unreachable!("Invalid default template")
     }
     tplm
